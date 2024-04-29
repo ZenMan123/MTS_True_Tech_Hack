a9093e83592ca79238f5e0b6813c4b5c810a4a59
samiaFife <anderbrine05@gmail.com>
2024-04-29 17:38:05 +0300

    added audio controller and frontend

--- backend/src/main/java/org/example/controller/TestController.java
+++ backend/src/main/java/org/example/controller/TestController.java
@@ -1,10 +1,23 @@
 package org.example.controller;
 
+
 import lombok.RequiredArgsConstructor;
 import org.example.dto.UserDto;
 import org.example.model.User;
 import org.example.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ws.schild.jave.Encoder;
+import ws.schild.jave.EncoderException;
+import ws.schild.jave.MultimediaObject;
+import ws.schild.jave.encode.AudioAttributes;
+import ws.schild.jave.encode.EncodingAttributes;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
 
 @RestController("api/test")
 @RequiredArgsConstructor
@@ -39,5 +52,31 @@ public class TestController {
     @PostMapping("userRegister")
     public void registerUser(@RequestBody UserDto user) {
         userRepository.registerUser(user);
+    }
+
+    @PostMapping("/upload-audio")
+    public String handleAudioUpload(@RequestParam("audio") MultipartFile audioFile) {
+        try {
+            File tempFile = File.createTempFile(audioFile.getOriginalFilename(), "-temp");
+            audioFile.transferTo(tempFile);
+            MultimediaObject multimediaObject = new MultimediaObject(tempFile);
+            tempFile.delete();
+            File outFile = File.createTempFile(audioFile.getOriginalFilename(), ".wav");
+            Encoder encoder = new Encoder();
+            EncodingAttributes attrs = new EncodingAttributes();
+            attrs.setOutputFormat("wav");
+            AudioAttributes audio = new AudioAttributes();
+            audio.setCodec("pcm_s16le");
+            attrs.setAudioAttributes(audio);
+            encoder.encode(multimediaObject, outFile, attrs);
+            AudioInputStream ais = AudioSystem.getAudioInputStream(outFile);
+            //обработка
+            return "Аудио успешно воспроизведено!";
+        } catch (UnsupportedAudioFileException | IOException e) {
+            e.printStackTrace();
+            return "Ошибка воспроизведения аудио: " + e.getMessage();
+        } catch (EncoderException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
