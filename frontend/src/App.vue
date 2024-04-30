81ccb1f59bf92c572a0f8c02a1bc2f696ca2133c
samiaFife <anderbrine05@gmail.com>
2024-04-30 22:55:53 +0300

    added vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <AppHeader/>
+  </div>
+</template>
+
+<script>
+
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
