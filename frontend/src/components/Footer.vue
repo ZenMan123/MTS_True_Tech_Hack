6cb8a8f76d4c5bf458890fc43c772b7163a0d878
olya.chekalina2004@gmail.com <olya.chekalina2004@gmail.com>
2024-05-03 16:58:22 +0500

    Create base footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -5,7 +5,17 @@ export default {
 </script>
 
 <template>
-  <p>Footer</p>
+  <div> 
+    <p>8 800 250-0890</p>
+    <p>Бесплатно для регионов РФ</p>
+    <a href="#" @click.prevent>© 2024 ПАО «МТС». Все права защищены</a><br>
+    <a href="#" @click.prevent>Поддержка</a><br>
+    <a href="#" @click.prevent>Соглашение о пользовании сервисом</a><br>
+    <a href="#" @click.prevent>Политика обработки cookies</a><br>
+    <a href="#" @click.prevent>Установите МТС Банк на смартфон</a><br>
+    <a href="#" @click.prevent>Без App Store и Google Play</a>
+  </div>
+  
 </template>
 
 <style scoped>
