5658cefef832b572225ac3b19130092a19a9ac31
iskbaga <112892889+iskbaga@users.noreply.github.com>
2024-05-11 18:24:06 +0300

    add classifier 2

---
+++ text_processing/text_classifier/text_classifier.py
@@ -0,0 +1,37 @@
+from transformers import AutoTokenizer, AutoModel
+import torch.nn.functional as F
+import torch
+
+from text_processing.config import EXAMPLES
+
+
+class TextClassifier:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny")
+        self.model = AutoModel.from_pretrained("cointegrated/rubert-tiny")
+        self.categorie_vectors = {}
+        for category, examples in EXAMPLES.items():
+            inputs = self.tokenizer(examples, padding=True, truncation=True, return_tensors="pt")
+            with torch.no_grad():
+                outputs = self.model(**inputs)
+                embeddings = outputs.last_hidden_state.mean(dim=1)
+                self.categorie_vectors[category] = embeddings.mean(dim=0)
+
+    def transform_feedback_to_vector(self, feedback):
+        inputs = self.tokenizer(feedback, padding=True, truncation=True, return_tensors="pt")
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+            comment_vector = outputs.last_hidden_state.mean(dim=1)
+        return comment_vector
+
+    def classify_feedback(self, feedback):
+        comment_vector = self.transform_feedback_to_vector(feedback)
+        distances = {}
+        for category, vector in self.categorie_vectors.items():
+            cosine_similarity = F.cosine_similarity(comment_vector, vector, dim=1).item()
+            distances[category] = cosine_similarity
+        max_category = max(distances, key=distances.get)
+        if distances[max_category] > 0.5:
+            return max_category
+        else:
+            return "другое"
