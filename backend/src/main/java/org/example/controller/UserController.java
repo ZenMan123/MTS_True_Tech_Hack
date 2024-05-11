54e03af8ba9d5391e8a7d1f64496f7905b054dd3
samiaFife <368983@niuitmo.ru>
2024-05-11 12:57:38 +0300

    changed controllers

--- backend/src/main/java/org/example/controller/UserController.java
+++ backend/src/main/java/org/example/controller/UserController.java
@@ -15,15 +15,13 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserController {
     private final JwtService jwtService;
-    private final UserService userService;
 
-    public UserController(JwtService jwtService, UserService userService) {
+    public UserController(JwtService jwtService) {
         this.jwtService = jwtService;
-        this.userService = userService;
     }
 
-    @GetMapping("users/auth")
-    public User findUserByJwt(@RequestParam String jwt) {
+    @GetMapping("/users/jwt")
+    public User findUserByJwt(@RequestParam(name = "jwt") String jwt) {
         return jwtService.find(jwt);
     }
 }
