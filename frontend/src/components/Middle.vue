ffd5cf1605c7541988c0dfe6bc309b8e6673bc88
samiaFife <368983@niuitmo.ru>
2024-05-13 00:05:14 +0300

    added button list

--- frontend/src/components/Middle.vue
+++ frontend/src/components/Middle.vue
@@ -7,13 +7,34 @@ export default {
   name: "Middle",
   props: ["user"],
   components: {Listener, MiddleGuest, MiddleLogged},
+  data() {
+    return {
+      buttonList: []
+    }
+  },
+  mounted() {
+    const links = document.querySelectorAll('a');
+    const buttons = document.querySelectorAll('button');
+    const elementsWithHoverEffect = document.querySelectorAll('.hover-effect');
+    const elements = [...links, ...buttons, ...elementsWithHoverEffect];
+    elements.forEach((button, index) => {
+      button.id = 'button_' + index;
+    });
+    this.buttonList = elements.map(element => {
+      return {
+        button_text: element.textContent || element.innerText,
+        button_id: element.id || null
+      };
+    });
+    console.log("Char " + this.buttonList)
+  }
 }
 </script>
 <template>
   <main>
     <MiddleLogged v-if="user" :user="user"/>
     <MiddleGuest v-else/>
-    <Listener/>
+    <Listener :buttonList="buttonList"/>
   </main>
 </template>
 
