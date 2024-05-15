18d8f3a7476971c4d0f1967cab400486baf0cf6a
RuslanHkmoff <ruslan.khackimov2016@gmail.com>
2024-05-16 02:21:08 +0300

    fixed ml_server

--- text_processing/config.py
+++ text_processing/config.py
@@ -1,7 +1,7 @@
 RUSSIAN_WORD2VEC_GENSIM_PATH = "word2vec-ruscorpora-300"
 RUSSIAN_WORD2VEC_LOCAL_PATH = "text_processing/models/russian_word2vec.wordvectors"
 
-THRESHOLD = 0.5
+THRESHOLD = 0.65
 
 CATEGORIES = [
     "запрос на отправку платежей",
